af0ec992436608c380c2dffebc800fc4dfae5b50
D7703_11 <d7703_11@d7703-11ui-imac.local>
2019-03-28 12:46:32 +0900

    TimerAlienImage

--- TimerTest03/ViewController.swift
+++ TimerTest03/ViewController.swift
@@ -10,11 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var AlienImage: UIImageView!
+    @IBOutlet weak var CountLable: UILabel!
+    
+    var count = 1
+    var timer = Timer() //timer 객체 생성
+    
+    var animating = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        AlienImage.image = UIImage(named:"Image1")
+        CountLable.text = String(count)
     }
 
+    
+    
+    
+    
+    @IBAction func Play(_ sender: Any) {
+        
+        if animating == true{return}
+        else{animating == true}
+        
+            timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
+         
+        
+    }
+    
+    
+    @IBAction func Pause(_ sender: Any) {
+        
+        
+            timer.invalidate()
+            animating = false;
+        
+        
+    }
+    
+    
+    @IBAction func Stop(_ sender: Any) {
 
+            timer.invalidate() //timer()를 멈춤
+            count = 0
+        
+        
+    }
+    
+    @objc func doAnimation(){  //selector로 연결
+        if(count == 5){
+            count = 0
+        }
+        count += 1
+        
+        AlienImage.image = UIImage(named: "Image\(count)")
+        CountLable.text = String(count)
+    }
 }
 
